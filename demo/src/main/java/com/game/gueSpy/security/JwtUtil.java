f2d0036dbcf61c69251320e9656c34a2af1af441
SaadWasim02 <saadwasim444@gmail.com>
2026-01-14 02:13:51 +0530

    added jwtUtil

---
+++ demo/src/main/java/com/game/gueSpy/security/JwtUtil.java
@@ -0,0 +1,48 @@
+package com.game.gueSpy.security;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class JwtUtil {
+    
+    @Value("${app.jwtSecret}")
+    private String jwtSecret;
+
+    public String generateToken(String username){
+        return Jwts.builder()
+            .setSubject(username)
+            .setIssuedAt(new Date())
+            .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+            .signWith(Keys.hmacShaKeyFor(jwtSecret.getBytes()), SignatureAlgorithm.HS256)
+            .compact();
+    }
+
+    public String extractUsername(String token){
+        return Jwts.parserBuilder()
+            .setSigningKey(jwtSecret.getBytes())
+            .build()
+            .parseClaimsJws(token)
+            .getBody()
+            .getSubject();
+    }
+
+    public boolean isTokenValid(String token){
+        try {
+            extractUsername(token);
+            log.info("{} is valid token", token);
+            return true;
+        } catch (Exception e) {
+            log.error("{} is not a valid token", token);
+            return false;
+        }
+    }
+}
