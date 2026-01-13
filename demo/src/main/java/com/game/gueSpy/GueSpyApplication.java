3c052390fb843854412c4b62894fab636e1fa7a8
SaadWasim02 <saadwasim444@gmail.com>
2026-01-14 01:26:55 +0530

    Project Setup

---
+++ demo/src/main/java/com/game/gueSpy/GueSpyApplication.java
@@ -0,0 +1,13 @@
+package com.game.gueSpy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GueSpyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GueSpyApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/game/gueSpy/GueSpyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.game.gueSpy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GueSpyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
